688ff11e5aedc2485ab9dd2e0a1ade838598eb06
liguoqinjim <liguoqinjim23@gmail.com>
2017-09-05 18:02:21 +0800

    modify filter

--- routers/filters.go
+++ routers/filters.go
@@ -7,7 +7,7 @@ import (
 
 var filterLoggedInUser = func(ctx *context.Context) {
 	url := ctx.Input.URL()
-	if strings.HasPrefix(url, "/session") || strings.HasPrefix(url, "/graphql") {
+	if strings.HasPrefix(url, "/session") || strings.HasPrefix(url, "/graphql") || strings.HasPrefix(url, "/query") {
 		return
 	}
 	_, ok := ctx.Input.Session("current_user").(map[string]string)
